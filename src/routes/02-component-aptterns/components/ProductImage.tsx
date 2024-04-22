200084104186d38fbb7aeceb3813abc500428193
Dario <dramirez@balanzashook.com.ar>
2024-04-22 15:37:35 -0300

    Listo

---
+++ src/routes/02-component-aptterns/components/ProductImage.tsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { ProductContext } from "./ProductCard";
+
+import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
+
+export const ProductImage = ({ img = '' }) => {
+  const { product } = useContext(ProductContext);
+  let imgShow: string;
+  if (img) {
+    imgShow = img;
+  } else if (product.img) {
+    imgShow = product.img;
+  } else {
+    imgShow = noImage;
+  }
+  return (
+    <img className={styles.productImg} src={imgShow} alt="Product image" />
+  )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,31 +1,32 @@
 import { BrowserRouter, Navigate } from "react-router-dom"
 import { Routes, Route, NavLink } from "react-router-dom";
 import logo from "../logo.svg";
+import { ShoppingPage } from "./02-component-aptterns/assets/ShoppingPage";
 
 export const Navigation = () => {
   return (
     <BrowserRouter>
       <div className="main-layout">
         <nav>
-          <img src={ logo } alt="logo" />
+          <img src={logo} alt="logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
+              <NavLink to="/shopping" className={({ isActive }) => isActive ? 'nav-active' : ''}>shopping</NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={ ({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Users</NavLink>
+              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="about" element={ <h1>About Page</h1> }/>
-          <Route path="users" element={ <h1>Users Page</h1> }/>
-          <Route path="home" element={ <h1>Home Page</h1> }/>
-          <Route path="/*" element={ <Navigate to="/home" replace /> }/>
+          <Route path="about" element={<h1>About Page</h1>} />
+          <Route path="users" element={<h1>Users Page</h1>} />
+          <Route path="shopping" element={<ShoppingPage />} />
+          <Route path="/*" element={<Navigate to="/shopping" replace />} />
 
         </Routes>
 
